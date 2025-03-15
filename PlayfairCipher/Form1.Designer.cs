26229c2c2128221d48917d24cb55290b6fc510d8
Viet-Hoang-2005 <tnvhoang2005@gmail.com>
2025-03-15 11:31:27 +0700

    Add PlayfiarCipher folder

---
+++ PlayfairCipher/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿using System.Windows.Forms;
+
+namespace PlayfairCipher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(330, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(170, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Playfair Cipher";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Text:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(54, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Key:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(591, 264);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 24);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Matrix";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(54, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Split:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(54, 339);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 18);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Result:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(136, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(298, 81);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(136, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(298, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(136, 261);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(298, 57);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(136, 339);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(603, 84);
+            this.textBox4.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(335, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeight = 4;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(502, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 4;
+            this.dataGridView1.RowTemplate.Height = 38;
+            this.dataGridView1.RowTemplate.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(237, 193);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ PlayfairCipher/Form1.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayfairCipher
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static void PlayfairCipher(string key, char[, ] a)
+        {
+            HashSet <char> mp = new HashSet<char>();
+            int k = 0, h = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    while (k < key.Length && mp.Contains(key[k]))
+                        k++;
+
+                    if (k < key.Length)
+                    {
+                        a[i, j] = key[k];
+                        mp.Add(key[k]);
+                        k++;
+                    }
+                    else
+                    {
+                        while (mp.Contains((char)('A' + h)) || (char)('A' + h) == 'J')
+                            h++;
+
+                        a[i, j] = (char)('A' + h);
+                        mp.Add((char)('A' + h));
+                        h++;
+                    }
+                }
+            }
+        }
+
+        static string Encryption(char x, char y, char[, ] a)
+        {
+            string res = "";
+            int i1 = -1, j1 = -1, i2 = -1, j2 = -1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (a[i,j] == x)
+                    {
+                        i1 = i;
+                        j1 = j;
+                    }
+                    else if (a[i, j] == y)
+                    {
+                        i2 = i;
+                        j2 = j;
+                    }
+                }
+            }
+            if (i1 == i2 && j1 != j2)
+            {
+                res += a[i1, (j1 + 1) % 5];
+                res += a[i2, (j2 + 1) % 5];
+            }
+            else if (j1 == j2 && i1 != i2)
+            {
+                res += a[(i1 + 1) % 5, j1];
+                res += a[(i2 + 1) % 5, j2];
+            }
+            else if (i1 != i2 && j1 != j2)
+            {
+                res += a[i1, j2];
+                res += a[i2, j1];
+            }
+            return res;
+        }
+
+        string Decryption(char x, char y, char[, ] a)
+        {
+            string res = "";
+            int i1 = -1, j1 = -1, i2 = -1, j2 = -1;
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (a[i, j] == x)
+                    {
+                        i1 = i;
+                        j1 = j;
+                    }
+                    else if (a[i, j] == y)
+                    {
+                        i2 = i;
+                        j2 = j;
+                    }
+                }
+            }
+            if (i1 == i2 && j1 != j2)
+            {
+                res += a[i1, (j1 - 1 + 5) % 5];
+                res += a[i2, (j2 - 1 + 5) % 5];
+            }
+            else if (j1 == j2 && i1 != i2)
+            {
+                res += a[(i1 - 1 + 5) % 5, j1];
+                res += a[(i2 - 1 + 5) % 5, j2];
+            }
+            else if (i1 != i2 && j1 != j2)
+            {
+                res += a[i1, j2];
+                res += a[i2, j1];
+            }
+            return res;
+        }
+        string upperString(string s)
+        {
+            string res = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsLetter(s[i]))
+                {
+                    if (s[i] == 'j' || s[i] == 'J')
+                        res += 'i';
+                    else
+                        res += s[i];
+                }
+            }
+            res = res.ToUpper();
+            return res;
+        }
+
+        string standardText(string s)
+        {
+            string res = "";
+            s = upperString(s);
+
+            for (int i = 0; i < s.Length; i += 2)
+            {
+                res += s[i];
+                if (i == s.Length - 1) break;
+                if (s[i] == s[i + 1])
+                {
+                    res += 'X';
+                    i--;
+                }
+                else res += s[i + 1];
+            }
+
+            if (res.Length % 2 != 0)
+                res += 'X';
+            return res;
+        }
+
+        List <string> SplitText(string text, string key, char[, ]a)
+        {
+            List <string> vt = new List<string>();
+
+            for (int i = 0; i < text.Length - 1; i += 2)
+                vt.Add(text[i].ToString() + text[i + 1].ToString());
+
+            return vt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            char[,] a = new char[5, 5];
+            List<string> vt = new List<string>();
+
+            textBox3.Clear();
+            textBox4.Clear();
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.RowCount = 5;
+
+            PlayfairCipher(upperString(textBox2.Text), a);
+            vt = SplitText(standardText(textBox1.Text), upperString(textBox2.Text), a);
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    dataGridView1.Rows[i].Cells[j].Value = a[i, j];
+            }
+            foreach (string x in vt)
+                textBox3.Text += x + " ";
+            foreach (string x in vt)
+                textBox4.Text += Encryption(x[0], x[1], a) + " ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            char[,] a = new char[5, 5];
+            List<string> vt = new List<string>();
+            
+            textBox3.Clear();
+            textBox4.Clear();
+
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            dataGridView1.ColumnCount = 5;
+            dataGridView1.RowCount = 5;
+            
+
+            PlayfairCipher(upperString(textBox2.Text), a);
+            vt = SplitText(standardText(textBox1.Text), upperString(textBox2.Text), a);
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                   dataGridView1.Rows[i].Cells[j].Value = a[i, j];
+            }
+            foreach (string x in vt)
+                textBox3.Text += x + " ";
+            foreach (string x in vt)
+                textBox4.Text += Decryption(x[0], x[1], a) + " ";
+        }
+    }
+}
