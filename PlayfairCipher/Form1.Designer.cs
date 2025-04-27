19095ef46471a50b3a83de7c8793396b77115c63
Viet-Hoang-2005 <tnvhoang2005@gmail.com>
2025-04-27 15:14:35 +0700

    Them button, radio button, sua kich thuoc form va ma tran

--- PlayfairCipher/Form1.Designer.cs
+++ PlayfairCipher/Form1.Designer.cs
@@ -43,6 +43,12 @@ namespace PlayfairCipher
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.button6 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -60,7 +66,7 @@ namespace PlayfairCipher
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(54, 69);
+            this.label2.Location = new System.Drawing.Point(30, 87);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(40, 18);
             this.label2.TabIndex = 1;
@@ -70,7 +76,7 @@ namespace PlayfairCipher
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(54, 166);
+            this.label3.Location = new System.Drawing.Point(33, 227);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 18);
             this.label3.TabIndex = 1;
@@ -80,17 +86,17 @@ namespace PlayfairCipher
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(591, 264);
+            this.label4.Location = new System.Drawing.Point(588, 59);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 24);
+            this.label4.Size = new System.Drawing.Size(97, 24);
             this.label4.TabIndex = 1;
-            this.label4.Text = "Matrix";
+            this.label4.Text = "Key matrix";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(54, 264);
+            this.label5.Location = new System.Drawing.Point(30, 327);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(40, 18);
             this.label5.TabIndex = 1;
@@ -100,7 +106,7 @@ namespace PlayfairCipher
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(54, 339);
+            this.label6.Location = new System.Drawing.Point(30, 400);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(54, 18);
             this.label6.TabIndex = 1;
@@ -108,41 +114,41 @@ namespace PlayfairCipher
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(136, 66);
+            this.textBox1.Location = new System.Drawing.Point(112, 86);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(298, 81);
+            this.textBox1.Size = new System.Drawing.Size(359, 88);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(136, 163);
+            this.textBox2.Location = new System.Drawing.Point(112, 226);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(298, 23);
+            this.textBox2.Size = new System.Drawing.Size(359, 23);
             this.textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(136, 261);
+            this.textBox3.Location = new System.Drawing.Point(112, 326);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox3.Size = new System.Drawing.Size(298, 57);
+            this.textBox3.Size = new System.Drawing.Size(359, 57);
             this.textBox3.TabIndex = 2;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(136, 339);
+            this.textBox4.Location = new System.Drawing.Point(112, 399);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
             this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox4.Size = new System.Drawing.Size(603, 84);
+            this.textBox4.Size = new System.Drawing.Size(652, 116);
             this.textBox4.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(136, 201);
+            this.button1.Location = new System.Drawing.Point(112, 267);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(100, 41);
             this.button1.TabIndex = 3;
@@ -152,7 +158,7 @@ namespace PlayfairCipher
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(335, 201);
+            this.button2.Location = new System.Drawing.Point(242, 267);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(99, 41);
             this.button2.TabIndex = 3;
@@ -165,20 +171,88 @@ namespace PlayfairCipher
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeight = 4;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.dataGridView1.Location = new System.Drawing.Point(502, 66);
+            this.dataGridView1.Location = new System.Drawing.Point(506, 86);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 4;
-            this.dataGridView1.RowTemplate.Height = 38;
+            this.dataGridView1.RowTemplate.Height = 36;
             this.dataGridView1.RowTemplate.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(237, 193);
+            this.dataGridView1.Size = new System.Drawing.Size(258, 222);
             this.dataGridView1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(372, 267);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 41);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Delete All";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(112, 52);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(77, 28);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Open file";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(112, 192);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(77, 28);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Open file";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(577, 324);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(51, 21);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "5x5";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(648, 324);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(51, 21);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "6x6";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(687, 365);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(77, 28);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Save file";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(807, 455);
+            this.ClientSize = new System.Drawing.Size(813, 527);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox4);
@@ -215,6 +289,12 @@ namespace PlayfairCipher
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Button button6;
     }
 }
 
